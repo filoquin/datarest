c00e68f142900f872a73458f623ab6c321f3b0e3
filoquin <filoquin@drriviera.(none)>
2015-06-24 15:20:11 -0300

    modified:   dataRest.py

--- dataRest.py
+++ dataRest.py
@@ -1,13 +1,17 @@
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 import pymssql
 import pydtt
 from dicttoxml import dicttoxml
 from flask import  Flask, url_for,  json, jsonify, request, Response
+from flask.ext.cors import CORS 
 from functools import wraps
-
-
+import decimal
+import datetime 
+import sys
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 def check_auth(username, password):
     return True
@@ -43,18 +47,87 @@ def return_result_set(result_set):
         return str.join(result_set)
 
     elif request.headers['Content-Type'] == 'application/json':
-        return json.dumps(result_set)
+        return json.dumps(result_set, default=decimal_default)
+
 
     elif request.headers['Content-Type'] == 'application/xml':
         return dicttoxml(result_set)
 
     else:
-        return json.dumps(result_set)
+        print result_set
+        return json.dumps(result_set,cls=EnhancedJSONEncoder)
+
+
+def decimal_default(obj):
+    if isinstance(obj, decimal.Decimal):
+        return float(obj)
+    '''if isinstance(obj, datetime.datetime):
+        return obj.strftime('%Y-%m-%d $H:%M:%S.%f')
+    	raise TypeError ("Type not serializable")
+    '''
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime('%Y-%m-%d %H:%M:%S.%f')
+            ARGS = ('year', 'month', 'day', 'hour', 'minute',
+                     'second', 'microsecond')
+            return {'__type__': 'datetime.datetime',
+                    'args': [getattr(obj, a) for a in ARGS]}
+        elif isinstance(obj, datetime.date):
+            return obj.strftime('%Y-%m-%d')
+            ARGS = ('year', 'month', 'day')
+            return {'__type__': 'datetime.date',
+                    'args': [getattr(obj, a) for a in ARGS]}
+        elif isinstance(obj, datetime.time):
+            return obj.strftime('%H:%M:%S.%f')
+            ARGS = ('hour', 'minute', 'second', 'microsecond')
+            return {'__type__': 'datetime.time',
+                    'args': [getattr(obj, a) for a in ARGS]}
+        elif isinstance(obj, datetime.timedelta):
+            return obj.strftime('%j%S%f')
+            ARGS = ('days', 'seconds', 'microseconds')
+            return {'__type__': 'datetime.timedelta',
+                    'args': [getattr(obj, a) for a in ARGS]}
+        elif isinstance(obj, decimal.Decimal):
+            return str(obj)
+        else:
+            return super().default(obj)
+
+
 
 @app.route('/')
 def api_root():
     ret ={'status':'ok'}
     return return_result_set(ret)
+
+
+'''
+''
+'' REST 
+'' Me permite acceder a RESTSERVEER
+'' 
+'' 
+''
+'''
+
+@app.route('/restserver/<parent>/<action>')
+def api_rest(parent,action):
+
+    
+    transaction = getattr(pydtt, "pydtt.pydtt_"+ parent)('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
+    print transaction
+
+    ret ={'status':'ok'}
+    return return_result_set(ret)
+    
+    return return_result_set(transaction.search())
+
+    return return_result_set(eval("transaction."+action))
+
+
+
+
 '''
 ''
 '' EMPRESAS
@@ -66,13 +139,38 @@ def api_root():
 
 @app.route('/empresas',methods = ['GET', 'POST'])
 def api_empresas():
-    transaction=pydtt.pydtt_empresas('host','db','usr','pass')
+    transaction=pydtt.pydtt_empresas('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
     return return_result_set(transaction.get_all())
 
 @app.route('/empresas/search')
 def api_search_empresas():
-    transaction=pydtt.pydtt_empresas('host','db','usr','pass')
+    transaction=pydtt.pydtt_empresas('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
     return return_result_set(transaction.search())
+
+'''
+''
+'' PACIENTES
+'' Me permite acceder a los registros de los pacienes
+'' 
+'' /pacientes/search
+''
+'''
+
+@app.route('/pacientes/search')
+def api_pacientes():
+    transaction=pydtt.pydtt_paciente('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
+    return return_result_set(transaction.search())
+
+@app.route('/pacientes/browse/<hlc>')
+def api_pacientes_browse(hlc):
+    transaction=pydtt.pydtt_paciente('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
+    return return_result_set(transaction.browse(hlc))
+
+@app.route('/pacientes/hc/browse/<hlc>')
+def api_pacientes_hc_browse(hlc):
+    transaction=pydtt.pydtt_paciente('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
+    return return_result_set(transaction.browse_hc(hlc))
+
 
 '''
 ''
@@ -85,12 +183,12 @@ def api_search_empresas():
 @app.route('/art',methods = ['GET', 'POST'])
 def api_art():
     
-    transaction=pydtt.pydtt_art('host','db','usr','pass')
+    transaction=pydtt.pydtt_art('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
     return return_result_set(transaction.get_all())
 
 @app.route('/art/search')
 def api_search_art():
-    transaction=pydtt.pydtt_art('host','db','usr','pass')
+    transaction=pydtt.pydtt_art('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
     return return_result_set(transaction.search())
 
 
@@ -106,10 +204,38 @@ def api_search_art():
 
 @app.route('/hc/ingresos/<from_time>')
 def api_hc_ingresos(from_time):
-    transaction=pydtt.pydtt_hc('host','db','usr','pass')
+    transaction=pydtt.pydtt_hc('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
     return return_result_set(transaction.get_from_time(from_time))
 
+@app.route('/hc/internados/<from_time>')
+def api_hc_internados(from_time):
+    transaction=pydtt.pydtt_hc('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
+    return return_result_set(transaction.get_internados_from_time(from_time))
 
+
+@app.route('/hc/insert_new_data',methods = ['GET', 'POST'])
+def api_hc_insert_new(from_time):
+    transaction=pydtt.pydtt_hc('POLICLI','PRUEBA_HC', 'PNSA\\meditim', 'riv$%250')
+    return return_result_set(transaction.api_hc_insert_new(from_time))
+
+
+@app.route('/hc/browse/<pac>')
+def api_hc_browse(pac):
+    transaction=pydtt.pydtt_hc('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
+    return return_result_set(transaction.browse(pac))
+
+
+@app.route('/laboral/empresas/all',methods = ['GET', 'POST'])
+def laboral_empresas_all():
+    transaction=pydtt.laboral('POLICLI','laboralsoft', 'PNSA\\meditim', 'riv$%250')
+    return return_result_set(transaction.empresas_all())
+
+
+@app.route('/crud/query',methods = ['GET', 'POST'])
+def api_crud():
+    transaction=pydtt.pydtt_crud('POLICLI','POLICLI', 'PNSA\\meditim', 'riv$%250')
+
+    return return_result_set(transaction.query())
 
 if __name__ == '__main__':
     app.run(host="10.0.5.41",port=5010)
--- pydtt.py
+++ pydtt.py
@@ -40,6 +40,32 @@ class pyddt(object):
 
         self.condition = op.join(condition)
 
+    def make_special_conditions(self,conditions):
+        condition = []
+        op = ' and '
+        keys =  self.active_parameters.keys()
+        for parameters in keys:
+            arg = request.args.get(parameters)
+            if arg:
+
+                if(self.active_parameters[parameters]['type']=="like"):
+                    condition.append("%s like '%%%s%%'" % (self.active_parameters[parameters]['field'], arg) )
+                elif(self.active_parameters[parameters]['type']=="ilike"):
+                    arg=arg.replace('*','%');
+                    condition.append("upper(%s) like upper('%%%s%%')" % (self.active_parameters[parameters]['field'], arg) )
+                elif(self.active_parameters[parameters]['type']=="startby"):
+                    condition.append("%s like '%s%%'" % (self.active_parameters[parameters]['field'], arg) )
+                elif(self.active_parameters[parameters]['type']=="number"):
+                    condition.append("%s =%f" % (self.active_parameters[parameters]['field'], arg) )
+                elif(self.active_parameters[parameters]['type']=="gdnu"):
+                    dnu=arg[1:]
+                    genero=arg[:1]
+                    condition.append("NDO =%f" % ( dnu) )
+                else : 
+                    condition.append("%s = '%s'" % (self.active_parameters[parameters]['field'], arg) )
+
+        self.condition = op.join(condition)
+
 
 
 
@@ -73,7 +99,7 @@ class pydtt_art(pyddt):
         self.cursor.execute("select INS, DES,DIR,LOC,CPO,CUI,NGA "
             "from dbo.CLINST "
             "where INS not like 'm%' and ISNUMERIC(INS)=1 and "
-            "( (convert (INT, pac.INS) > 4300 and convert (INT, pac.INS) < 4999) "
+            " (convert (INT, INS) > 4300 and convert (INT,INS) < 4999) "
         )
 
         return self.cursor.fetchall()
@@ -90,13 +116,14 @@ class pydtt_art(pyddt):
         return self.cursor.fetchall()
 
 class pydtt_hc(pyddt):
-    active_parameters={'nombre':'pac.NOM','HCL':'hcl.HCL'}
+    active_parameters={'nombre':{'field':'NOM','type':'ilike' },'dnu':{'field':'NDO','type':'startby' },'hcl':{'field':'HCL','type':'number' },'pac':{'field':'PAC','type':'number' }}
 
     def get_from_time(self,start_time):
-        self.cursor.execute("select  ORI, PAC , pac.NOM , pac.DIR , pac.LOC, pac.CPO, pac.TEL, pac.FNA, pac.SEX,  pac.NDO, "
-            "pac.INS, pac.HCL , FIN "
+        self.cursor.execute("select  ORI, PAC , pac.NOM , pac.DIR , pac.LOC, pac.CPO, pac.TEL, convert(varchar, pac.FNA , 111) as FNA, pac.SEX,  pac.NDO, "
+            "pac.INS, pac.HCL ,  convert(varchar,FIN, 120) as FIN , l.DES as localidad "
             "from CLIPAC pac "
             "left join CLIHCL hcl on (pac.HCL = hcl.HCL ) "
+            "left join CLILOC l on (l.LOC = pac.LOC ) "
             "where ORI='A' "
             "and FIN>= '%s' and ISNUMERIC(pac.INS)=1 and "
             "( (convert (INT, pac.INS) > 4300 and convert (INT, pac.INS) < 4999) ) " % (start_time)
@@ -104,42 +131,133 @@ class pydtt_hc(pyddt):
 
         return self.cursor.fetchall()
 
-    def search(self):
+    def get_internados_from_time(self,start_time):
+        
+        self.cursor.execute("select  pac.* "
+            "from CLIPAC pac "
+            "left join CLIHCL hcl on (pac.HCL = hcl.HCL ) "
+            "where ORI='I' "
+            "and FIN>= '%s' " % (start_time)
+        )
 
-        self.cursor.execute("select INS, DES,DIR,LOC,CPO,CUI,NGA "
-            "from dbo.CLINST "
-            "where INS not like 'm%' and ISNUMERIC(INS)=1 and "
-            "( (convert (INT, pac.INS) > 4300 and convert (INT, pac.INS) < 4999) "
-            "and DES like '%(cond)s'"  % filters
+        return self.cursor.fetchall()
+    def search(self):
+        self.make_special_conditions(request.args)
+
+        self.cursor.execute("select top 100 NOM, NDO , HCL , PAC "
+            "from CLIPAC pac "
+            "where " + self.condition 
         )
         return self.cursor.fetchall()
+
+    def browse(self,pac):
+        self.make_special_conditions(request.args)
+        result_set=[]
+        self.cursor.execute("select top 1 * "
+            "from CLIPAC pac "
+            "where " + self.condition 
+        )
+
+        for row in self.cursor.fetchall():
+            item=row
+            self.cursor.execute("select  tipo.DES , preg.LEY_1 , ope.NOM as operador ,ev.* "
+                        "from CNSRSP  ev "
+                        "join CNSANA  tipo on (ev.ANA=tipo.ANA) "
+                        "join CNSPRG preg on (ev.PRG=preg.PRG) "
+                        "join SYSOPE ope on (ev.OPE=ope.OPE) "
+                        "where ev.PAC=%d" % (row['PAC']))
+            item['preguntas']=self.cursor.fetchall()
+            result_set.append(item)
+        return result_set
+
+    def api_hc_insert_new(self):
+        pass
+    def get_paciente(self,paciente):
+        hcl = self.cursor.execute("select  HCL from CLIHCL where SEX='%s' NDO='%f'"%(paciente['genero'],paciente['dnu'])).fetchone()
+        if (hcl is None):
+            self.create_hcl(paciente)
+        else : 
+            return hcl['HCL']
+    def create_paciente(self,paciente):
+        pass
+    def next_geneador(self,codigo):
+        pass
+        
+
+
+class laboral(pyddt):
+
+    def empresas_all(self):
+        self.cursor.execute("select  razonsocial,  CUIT, domicilio from empresas where activa =1" )
+        return self.cursor.fetchall()
+
 
 
 
 class pydtt_paciente(pyddt):
-    active_parameters={'nombre':'pac.NOM','HCL':'hcl.HCL'}
-
-    def get_from_time(self,start_time):
-        self.cursor.execute("select  ORI, PAC , pac.NOM , pac.DIR , pac.LOC, pac.CPO, pac.TEL, pac.FNA, pac.SEX,  pac.NDO, "
-            "pac.INS, pac.HCL , FIN "
-            "from CLIPAC pac "
-            "left join CLIHCL hcl on (pac.HCL = hcl.HCL ) "
-            "where ORI='A' "
-            "and FIN>= '%s' and ISNUMERIC(pac.INS)=1 and "
-            "( (convert (INT, pac.INS) > 4300 and convert (INT, pac.INS) < 4999) ) " % (start_time)
-        )
-
-        return self.cursor.fetchall()
+    active_parameters={'nombre':{'field':'NOM','type':'ilike' },'dnu':{'field':'NDO','type':'startby' },'hcl':{'field':'HCL','type':'number' }}
 
     def search(self):
-
-        self.cursor.execute("select INS, DES,DIR,LOC,CPO,CUI,NGA "
-            "from dbo.CLINST "
-            "where INS not like 'm%' and ISNUMERIC(INS)=1 and "
-            "( (convert (INT, pac.INS) > 4300 and convert (INT, pac.INS) < 4999) "
-            "and DES like '%(cond)s'"  % filters
+        self.make_special_conditions(request.args)
+        self.cursor.execute("select top 100 NOM, NDO , HCL "
+            "from CLIHCL "
+            "where " + self.condition 
         )
         return self.cursor.fetchall()
 
+    def browse(self,hcl):
+        #super('pydtt_paciente', self).active_parameters={'dnu':{'field':'NDO','type':'string' },'hcl':{'field':'HCL','type':'number' }}
+        #self.make_special_conditions(request.args)
+        self.cursor.execute("select top 1 * "
+            "from CLIHCL "
+            "where HCL=%d" % (hcl) 
+        )
+        return self.cursor.fetchall()
+    
+    def browse_hc(self,hcl):
+        #super('pydtt_paciente', self).active_parameters={'dnu':{'field':'NDO','type':'string' },'hcl':{'field':'HCL','type':'number' }}
+        #self.make_special_conditions(request.args)
+        result_set={}
+        print ("select * "
+            "from CLIHCL "
+            "where HCL=%s" % (hcl) 
+        )
+        self.cursor.execute("select * "
+            "from CLIHCL "
+            "where HCL=%s" % (hcl) 
+        )
+        result_set['paciente']=self.cursor.fetchone()
+        result_set['atenciones']=[]
+
+        self.cursor.execute("select  * "
+            "from CLIPAC pac "
+            "where HCL=%s" %(hcl) 
+        )
+
+        for row in self.cursor.fetchall():
+            item=row
+            self.cursor.execute("select  tipo.DES ,  ope.NOM as operador , ev.FEC , ev.ORD,preg.LEY_1, "
+                        "CASE  WHEN RSP_NUM  IS NULL THEN  TXT_LIB ELSE rsp.DES END  as respuesta "
+                        "from CNSRSP  ev "
+                        "join CNSANA  tipo on (ev.ANA=tipo.ANA) "
+                        "join CNSPRG preg on (ev.PRG=preg.PRG) "
+                        "left join CNSRPO rsp on (ev.PRG=rsp.PRG and ev.RSP_NUM=rsp.RSP) "
+                        "join SYSOPE ope on (ev.OPE=ope.OPE) "
+                        "where ev.PAC=%d" % (row['PAC']))
+            item['preguntas']=self.cursor.fetchall()
+            result_set['atenciones'].append(item)
+        return result_set
 
 
+class pydtt_crud(pyddt):
+
+    def query(self):
+        query = str(request.form['q'])
+        self.cursor.execute(query)
+        result_set= self.cursor.fetchall()
+        return result_set
+'''select  tipo.DES , preg.LEY_1 ,ev.*
+ from CNSRSP  ev 
+join CNSANA  tipo on (ev.ANA=tipo.ANA)
+join    CNSPRG preg on (ev.PRG=preg.PRG)
+ where ev.FEC > '2015-5-11' and OPE=443'''
